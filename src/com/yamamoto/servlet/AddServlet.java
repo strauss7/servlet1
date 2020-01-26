914809060661328c54342ba603d975db01014202
strauss7 <wbsabbyyuji@gmail.com>
2020-01-26 15:20:26 -0300

    first commit

---
+++ src/com/yamamoto/servlet/AddServlet.java
@@ -0,0 +1,27 @@
+package com.yamamoto.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/AddServlet")
+public class AddServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i + j;
+		
+		PrintWriter out = resp.getWriter();
+		
+		out.println("Result is " + k);
+	}
+}
